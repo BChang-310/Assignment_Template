9b8e21e70f61042c3e48372a29231cda8875213e
BChang-310 <89481721+BChang-310@users.noreply.github.com>
2021-08-25 14:32:46 -0500

    Update convert_nb.py

--- tests/convert_nb.py
+++ tests/convert_nb.py
@@ -1,11 +1,12 @@
 import nbconvert
 import glob
 
-assignment_nb = glob.glob('../*.ipynb')[0]
+if __name__ == "__main__":
+    assignment_nb = glob.glob('../*.ipynb')[0]
 
-with open(assignment_nb) as f:
-    exporter = nbconvert.PythonExporter()
-    python_file, _ = exporter.from_file(f)
+    with open(assignment_nb) as f:
+        exporter = nbconvert.PythonExporter()
+        python_file, _ = exporter.from_file(f)
 
-with open("assignment.py", "w") as f:
-    f.write(python_file)
+    with open("assignment.py", "w") as f:
+        f.write(python_file)
