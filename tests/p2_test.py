70417fee5effee31e3d5b6bba675815c0b764d45
BChang-310 <89481721+BChang-310@users.noreply.github.com>
2021-08-25 14:48:27 -0500

    Update p2_test.py

--- tests/p2_test.py
+++ tests/p2_test.py
@@ -1,9 +1,10 @@
-from assignment import problem_2
-
+from convert_nb import convert
 import unittest
 
 import numpy as np
 
+convert()
+from assignment import problem_2
 class TestSolution(unittest.TestCase):
     def test_prob2(self):
         np.testing.assert_equal(problem_2(2, 4), 6)
