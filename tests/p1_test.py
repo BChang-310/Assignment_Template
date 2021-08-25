111ce9d31599caea056e2e618ae97b3d7e666b03
BChang-310 <89481721+BChang-310@users.noreply.github.com>
2021-08-25 15:09:04 -0500

    Add files via upload

---
+++ tests/p1_test.py
@@ -0,0 +1,20 @@
+import unittest
+
+import numpy as np
+
+with open(assignment_nb) as f:
+    exporter = nbconvert.PythonExporter()
+    python_file, _ = exporter.from_file(f)
+
+with open("assignment.py", "w") as f:
+    f.write(python_file)
+
+from assignment import problem_1
+
+class TestSolution(unittest.TestCase):
+    def test_prob1(self):
+        np.testing.assert_string_equal(problem_1(), "Hello, world!")
+
+
+if __name__ == '__main__':
+    unittest.main()
